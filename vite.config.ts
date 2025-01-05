992a834f3457be4b0ef1661114459b0ca0833fdc
Steven Cleveland <steven.cleveland41@.yahoo.com>
2025-01-05 17:07:37 +0100

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/QuizApp/',
+  base: "/QuizApp/",
   plugins: [react()],
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
